d0ad08d4a53d1c57daf012b837a934e0c7acbf1f
liamcclane <lia_mcclane@msn.com>
2020-10-16 13:00:29 -0700

    added class files

---
+++ src/Address.java
@@ -0,0 +1,32 @@
+public class Address {
+
+	private int id;
+	private String address;
+	private String state;
+	public Address(int id, String address, String state) {
+		super();
+		this.id = id;
+		this.address = address;
+		this.state = state;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	
+}
