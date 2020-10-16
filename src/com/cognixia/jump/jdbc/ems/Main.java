75f9284f46e7cc96824003b5ef29aa1fd3b0b6b5
Daniel Imaino <cryptodimaino@gmail.com>
2020-10-16 13:56:43 -0700

    Added folders and java files needed

--- src/com/cognixia/jump/jdbc/ems/Main.java
+++ src/com/cognixia/jump/jdbc/ems/Main.java
@@ -3,8 +3,7 @@ package com.cognixia.jump.jdbc.ems;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
---
+++ src/com/cognixia/jump/jdbc/ems/junit/DepartmentImpTest.java
@@ -0,0 +1,14 @@
+package com.cognixia.jump.jdbc.ems.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DepartmentImpTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
