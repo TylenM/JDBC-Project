b1b00e2e9bb466ce7a56f803418fe8956eda6cbc
Tylen Miller <millert@Tylens-MBP.attlocal.net>
2020-10-16 15:29:03 -0500

    Updated File

--- src/com/cognixia/jump/jdbc/ems/EMSResultSets.java
+++ src/com/cognixia/jump/jdbc/ems/EMSResultSets.java
@@ -14,7 +14,13 @@ public class EMSResultSets {
 		
 		try ( Connection conn = ConnectionManager.getConnection();
 			  Statement stmt = conn.createStatement();
-			  ResultSet rs = stmt.executeQuery("select * from employee");){
+			  ResultSet rs = stmt.executeQuery("select * from employee");
+			  Statement stmt2 = conn.createStatement();
+			  ResultSet rs2 = stmt2.executeQuery("select * from department");
+			  Statement stmt3 = conn.createStatement();
+			  ResultSet rs3 = stmt3.executeQuery("select * from address");
+			  Statement stmt4 = conn.createStatement();
+			  ResultSet rs4 = stmt4.executeQuery("select * from company");){
 
 			
 			System.out.println("\n\nEmployees");
@@ -23,7 +29,7 @@ public class EMSResultSets {
 			while(rs.next()) {
 				
 				int employee_id = rs.getInt("employee_id");
-				String name =  rs.getString("first_name") + " " + rs.getString("last_name");
+				String employee_name =  rs.getString("first_name") + " " + rs.getString("last_name");
 				Date dob = rs.getDate("date_of_birth");
 				String email = rs.getString("email");
 				String phone = rs.getString("phone");
@@ -32,16 +38,56 @@ public class EMSResultSets {
 				int address_id = rs.getInt("address_id");
 				int company_id = rs.getInt("comapny_id");
 				
-				System.out.println("ID: " + employee_id + " || Name: " + name + " || Date of Birth: " + dob + " || Email: " + email + " || Phone: " + phone + " || Phone: " + salary + " || Department ID: " + department_id + " || Address ID: " + address_id + " || Company ID: " + company_id);
+				System.out.println("ID: " + employee_id + " || Name: " + employee_name + " || Date of Birth: " + dob + " || Email: " + email + " || Phone: " + phone + " || Phone: " + salary + " || Department ID: " + department_id + " || Address ID: " + address_id + " || Company ID: " + company_id);
+				
+			}
+			rs.close();
+			
+			System.out.println("\n\nDepartments");
+			System.out.println("------------------------");
+			while(rs2.next()) {
+				
+				int department_id = rs2.getInt("department_id");
+				String department_name =  rs2.getString("name");
+				Double budget = rs2.getDouble("budget");
+				String ext = rs2.getString("phone_extension");
+				
+				System.out.println("ID: " + department_id + " || Name: " + department_name + " || Budget: " + budget + " || Phone Extension: " + ext);
 				
 			}
 			
+			rs2.close();
 			
+			System.out.println("\n\nAddress");
+			System.out.println("------------------------");
+			while(rs3.next()) {
+				
+				int address_id = rs3.getInt("address_id");
+				String address =  rs3.getString("address");
+				String state = rs3.getString("state");
+				
+				System.out.println("ID: " + address_id + " || Address: " + address + " || State: " + state);
+				
+			}
+			
+			rs3.close();
+			
+			System.out.println("\n\nCompany");
+			System.out.println("------------------------");
+			while(rs4.next()) {
+	
+				int company_id = rs4.getInt("company_id");
+				String company_name =  rs4.getString("name");
+				
+				System.out.println("ID: " + company_id + " || Name: " + company_name);
+				
+			}
+
+			rs4.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
+		
 		}
-		
-		
 	}
 
 }
