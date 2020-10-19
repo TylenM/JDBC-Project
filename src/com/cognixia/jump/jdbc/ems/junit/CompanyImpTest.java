15282607d69b0993b78bf591e2b2b2654d5d6a09
liamcclane <lia_mcclane@msn.com>
2020-10-19 09:44:49 -0700

    added ordering to Company J-Unit file

--- src/com/cognixia/jump/jdbc/ems/junit/CompanyImpTest.java
+++ src/com/cognixia/jump/jdbc/ems/junit/CompanyImpTest.java
@@ -4,33 +4,41 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import com.cognixia.jump.jdbc.ems.implementations.*;
 import com.cognixia.jump.jdbc.ems.models.Company;
 
+
+@TestMethodOrder(OrderAnnotation.class)
 class CompanyImpTest {
 
 	CompanyImp compImp = new CompanyImp();
-	String addedString = "WHATEVER 1,2";
-	String editName = "WHATEVER 112";
+	String addedString = "Something";
+	String editName = "some!!";
 	
 	@Test
+	@Order(1)
 	void testAddCompany() {
 		boolean b = compImp.addCompany(new Company(22, addedString));
 		assertTrue(b);	
 	}
 	@Test
+	@Order(2)
 	void testFindAll() {
 		assertNotNull(compImp.findall());
 	}
 	@Test 
+	@Order(3)
 	void findByName() {
 		Company c = compImp.findOneByName(addedString);
-		System.out.println("c" + c);
 		assertEquals(c.getName(), addedString);
 	}
 	@Test
+	@Order(4)
 	void testEditCompany() {
 		Company editToThis = compImp.findOneByName(addedString);
 		editToThis.setName(editName);
@@ -38,6 +46,7 @@ class CompanyImpTest {
 		assertTrue(b);
 	}
 	@Test
+	@Order(5)
 	void testDeleteCompany() {
 		Company lastestCompany = compImp.findOneByName(editName);
 		boolean b = compImp.deleteCompany(lastestCompany);
