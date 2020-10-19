12d063b030b23f61590e59fb6120ee761151f52b
liamcclane <lia_mcclane@msn.com>
2020-10-19 09:14:48 -0700

    J-Unit Company test in progress

--- src/com/cognixia/jump/jdbc/ems/junit/CompanyImpTest.java
+++ src/com/cognixia/jump/jdbc/ems/junit/CompanyImpTest.java
@@ -4,8 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import com.cognixia.jump.jdbc.ems.implementations.*;
@@ -14,43 +12,37 @@ import com.cognixia.jump.jdbc.ems.models.Company;
 class CompanyImpTest {
 
 	CompanyImp compImp = new CompanyImp();
-	String addedString = "TESTING 1,2";
-	String editName = "TEsting 112";
+	String addedString = "WHATEVER 1,2";
+	String editName = "WHATEVER 112";
 	
-	@Before
+	@Test
 	void testAddCompany() {
-		Company dummy = new Company(22, addedString);
-		boolean b = compImp.addCompany(dummy);
+		boolean b = compImp.addCompany(new Company(22, addedString));
 		assertTrue(b);	
 	}
 	@Test
 	void testFindAll() {
-		List<Company> arr = compImp.findall();
-		assertNotNull(arr);
+		assertNotNull(compImp.findall());
 	}
 	@Test 
 	void findByName() {
 		Company c = compImp.findOneByName(addedString);
+		System.out.println("c" + c);
 		assertEquals(c.getName(), addedString);
 	}
 	@Test
 	void testEditCompany() {
-		Company editToThis = new Company(compImp.findOneByName(addedString).getId(),editName);
+		Company editToThis = compImp.findOneByName(addedString);
+		editToThis.setName(editName);
 		boolean b = compImp.updateCompany(editToThis);
 		assertTrue(b);
 	}
-	@After
+	@Test
 	void testDeleteCompany() {
 		Company lastestCompany = compImp.findOneByName(editName);
 		boolean b = compImp.deleteCompany(lastestCompany);
 		assertTrue(b);
 	}
 	
-//	@Test
-//	void testDeleteCompanyById() {
-//		int lastestCompanyId = compImp.findall().get(compImp.findall().size() - 1).getId();
-//		boolean b = compImp.deleteCompanyById(lastestCompanyId);
-//		assertTrue(b);
-//	}
 
 }
