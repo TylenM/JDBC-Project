7800d019993016da8557d2e331bd20d256144fbb
Tylen Miller <millert@Tylens-MBP.attlocal.net>
2020-10-19 11:11:49 -0500

    update AddressDAO

--- src/com/cognixia/jump/jdbc/ems/interfaces/AddressDAO.java
+++ src/com/cognixia/jump/jdbc/ems/interfaces/AddressDAO.java
@@ -1,5 +1,26 @@
 package com.cognixia.jump.jdbc.ems.interfaces;
 
+import java.util.List;
+
+import com.cognixia.jump.jdbc.ems.Address;
+import com.cognixia.jump.jdbc.exceptions.AddressNotFoundException;
+
 public interface AddressDAO {
+
+	
+	public List<Address> getAllAddresses();
+	
+	public Address getAddressById(int id);
+	
+	public Address getAddressByState(String state);
+	
+	public Address getAddressByName(String address) throws AddressNotFoundException;
+	
+	public boolean addAddress(Address address);
+	
+	public boolean deleteAddress(int id);
+	
+	public boolean updateAddress(Address address);
+
 
 }
